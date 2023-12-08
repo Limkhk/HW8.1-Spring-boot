5d0b1d13dc8c020e9f398db9199230b6afc9d946
Averkin S.S <ssaverkin@ya.ru>
2023-12-08 20:42:44 +0300

    init commit

---
+++ src/main/java/ru/netology/Application.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+
+}
